76176f4f0f7a75b3c42bb8639f24edafd92563ac
dante.romero <dante.romero@tecso.coop>
2020-11-08 17:52:08 -0300

    initial commit

---
+++ src/main/java/com/tecso/jwt/models/Role.java
@@ -0,0 +1,35 @@
+package com.tecso.jwt.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
+
+	public Role() { }
+
+	public Role(ERole name) {
+		this.name = name;
+	}
+
+}
